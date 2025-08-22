10d3c64e7465deaaa5d67f922bb39b9f821cffce
Ajay Joshi <joshiajay311@gmail.com>
2025-08-22 22:05:53 +0530

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,37 @@
+const products = [
+  { id: 1, name: "Macbook", price: 80000 },
+  { id: 2, name: "Sofa-set", price: 55000 },
+  { id: 3, name: "Smart-Phone", price: 120000 },
+  { id: 4, name: "Ipad", price: 32000 },
+  { id: 5, name: "Wall-clock", price: 3000 },
+];
+
+function Products() {
+  return (
+    <div>
+      <h1>Product Catalog</h1>
+
+      <ul style={{ listStyleType: "none" }}>
+        {products.map((product) => (
+          <li
+            style={{
+              border: "1px solid black",
+              maxWidth: "300px",
+              borderRadius: "5px",
+              marginBottom: "5px",
+              padding: "5px",
+            }}
+            key={product.id}
+          >
+            <span style={{ marginRight: "20px" }}>
+              {product.name} - INR {product.price}
+            </span>
+            <button>Add to Cart</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
